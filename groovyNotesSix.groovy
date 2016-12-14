efc2b11e90d02fdb7d1d596c7eae62cfc6c275cf
nbkhope <ryumemaru@yahoo.com>
2016-12-13 17:05:29 -0800

    Add sumNumbers() and factorial()

--- groovyNotesSix.groovy
+++ groovyNotesSix.groovy
@@ -5,6 +5,10 @@ class groovyNotesSix {
     def numbers = [1, 2, 3, 4, 5]
     println numbers
     println doubleNumbers(numbers)
+
+    println sumNumbers(4, 3, 7)
+
+    println factorial(4) // 4 * 3 * 2 * 1 = 24
   }
 
   // Defining a method
@@ -21,5 +25,18 @@ class groovyNotesSix {
 
     // using a closure (this is like "map()")
     list.collect { it * 2 }
+  }
+
+  static def sumNumbers(int... numbers) {// note the use of ... for a variable number of arguments
+    def sum = 0
+    // closures again
+    numbers.each { sum += it }
+    sum
+  }
+
+  static def factorial(number) {
+    number <= 1 ? 1 : number * factorial(number - 1)
+
+    // there's no "return 1 if number <= 1" in Groovy 
   }
 }
